136c3c3f676cdd08aef69d2398a039f7cd05ad5b
Stanislav Slavkov <s.slavkov20@gmail.com>
2023-03-23 15:19:58 +0200

    Updated functionality

--- resources/script.js
+++ resources/script.js
@@ -9,31 +9,21 @@ async function compressImage(blobImg, percent) {
     return dataUrl;
   }
   
-   document.getElementById('upload').addEventListener('change', async(e) => {
+
+    document.querySelector('.compress-btn').addEventListener('click', async() => {
     document.querySelector('.error_message').innerHTML = ""
     document.querySelector('.success_message').innerHTML = ""
     document.querySelector('.blink_me').innerHTML = ""
     document.querySelectorAll('.with_scroll').forEach(element => {
       element.scrollTo(0, 0);
     });
-    let img = e.target.files[0];
-    if(img.type != "image/png" && img.type != "image/jpeg"){
-      document.querySelector('.error_message').innerHTML = "Error. Only png and jpeg files are accepted!"
+    let img = document.getElementById('upload').files[0];
+   if(img == null || img.type != "image/png" && img.type != "image/jpeg"){
+      document.querySelector('.error_message').innerHTML = "Upload an image first!"
     }
     else{
       
     document.querySelector('.blink_me').innerHTML = "Compressing ..."
-    let uploadedHTML = `<li class="row">
-                            <div class="content upload">
-                              <i class="fas fa-file-alt"></i>
-                              <div class="details">
-                                <span class="name">${img.name} • Uploaded</span>
-                                <span class="size">${formatBytes(parseFloat(img.size.toLocaleString().replace(/,/g, "")))}</span>
-                              </div>
-                            </div>
-                            <i class="fas fa-check"></i>
-                          </li>`;
-                          document.querySelector('.uploaded-area').insertAdjacentHTML("afterbegin", uploadedHTML);
     
     let imgCompressed = await compressImage(img, 30) // set to 75%
     let compSize = atob(imgCompressed.split(",")[1]).length;
@@ -68,13 +58,17 @@ var objectURL = URL.createObjectURL(blob);
                               <i class="fas fa-file-alt"></i>
                               <div class="details">
                                 <span class="name">${img.name} • Ready</span>
-                                <span class="size">${formatBytes(parseFloat(compSize.toLocaleString().replace(/,/g, "")))}<a href="${objectURL}" download=${img.name} style='text-decoration: none; cursor: url("hand.png"), auto;' class='text-success ms-3'>Click to download</a></span>
+                                <span class="size">${formatBytes(parseFloat(compSize.toLocaleString().replace(/,/g, "")))}<a href="${objectURL}" download=${img.name} style='text-decoration: none; cursor: url("resources/hand.png"), auto;' class='text-success ms-3'>Click to download</a></span>
                               </div>
                             </div>
                             <i class="fas fa-check"></i>
                           </li>`;
                           document.querySelector('.ready-to-download-area').insertAdjacentHTML("afterbegin", downloadHTML);
                           document.querySelector('.success_message').innerHTML = "Compression finished!"
+                          if(document.querySelector('.uploadedImage') != null)
+  {  document.querySelector('.uploadedImage').remove();
+  document.getElementById('upload').value = ""}
+
   }});
 
   function formatBytes(a,b=2){if(!+a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return`${parseFloat((a/Math.pow(1024,d)).toFixed(c))} ${["Bytes","KiB","MiB","GiB","TiB","PiB","EiB","ZiB","YiB"][d]}`}
@@ -82,3 +76,26 @@ var objectURL = URL.createObjectURL(blob);
    document.querySelector(".file-input").click();
   
 });
+
+document.getElementById('upload').addEventListener('change',function(){
+  let imgType = document.getElementById('upload').files[0];
+  if(imgType.type != "image/png" && imgType.type != "image/jpeg"){
+    document.querySelector('.error_message').innerHTML = "Error. Only png and jpeg files are accepted!"
+  }
+  else{
+  let img = document.getElementById('upload').files[0];
+  if(document.querySelector('.uploadedImage') != null)
+  {  document.querySelector('.uploadedImage').remove();}
+  let uploadedHTML = `<li class="row uploadedImage">
+  <div class="content upload">
+    <i class="fas fa-file-alt"></i>
+    <div class="details">
+      <span class="name">${img.name} • Uploaded</span>
+      <span class="size">${formatBytes(parseFloat(img.size.toLocaleString().replace(/,/g, "")))}</span>
+    </div>
+  </div>
+  <i class="fas fa-check"></i>
+</li>`;
+document.querySelector('.uploaded-area').insertAdjacentHTML("afterbegin", uploadedHTML);
+}
+})
